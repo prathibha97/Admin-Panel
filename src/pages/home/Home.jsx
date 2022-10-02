952b8dfbfbe976aff9000d8e9989e799882eaa3e
Prathibha <prsthibha@gmail.com>
2022-10-02 15:20:32 +0530

    Add widgets and styles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,23 @@
-import Sidebar from '../../components/sidebar/Sidebar'
-import './Home.scss';
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
+import "./Home.scss";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Sidebar/>
-      <div className="homeContainer">containers</div>
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
