a5b6666e97420c010bd87bfc93217b14450b1e1a
Prathibha <prsthibha@gmail.com>
2022-10-02 16:12:53 +0530

    Add chart component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
+import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget";
+import Chart from "../../components/chart/Chart";
 import "./Home.scss";
 
 const Home = () => {
@@ -14,6 +16,10 @@ const Home = () => {
           <Widget type="order" />
           <Widget type="earnings" />
           <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
         </div>
       </div>
     </div>
