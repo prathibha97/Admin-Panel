b9bdf6b9de5b8ea710ab85d089cf174054a8ca0a
Prathibha <prsthibha@gmail.com>
2022-10-02 19:53:51 +0530

    Delete user function

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -2,20 +2,31 @@ import "./DataTable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const DataTable = () => {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
   const actionsColumn = [
     {
       field: "action",
       headerName: "Action",
       width: 200,
-      renderCell: () => {
+      renderCell: (params) => {
         return (
           <div className="cellAction">
             <Link to="/users/test" style={{ textDecoration: "none" }}>
               <div className="viewButton">View</div>
             </Link>
-            <div className="deleteButton">Delete</div>
+            <div
+              className="deleteButton"
+              onClick={() => handleDelete(params.row.id)}
+            >
+              Delete
+            </div>
           </div>
         );
       },
@@ -29,8 +40,9 @@ const DataTable = () => {
           Add New
         </Link>
       </div>
-      <DataGrid className="datagrid"
-        rows={userRows}
+      <DataGrid
+        className="datagrid"
+        rows={data}
         columns={userColumns.concat(actionsColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
